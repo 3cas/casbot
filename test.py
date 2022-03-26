81996e10970a5fc6ad6f9e481d41ad7e6035653f
CAS <78571139+cas-14@users.noreply.github.com>
2022-03-26 17:32:21 -0400

    fix again

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-r = requests.post(
-    "https://api.deepai.org/api/text-generator",
-    data={'text': "green"},
-    headers={'api-key': "cd7d7e11-252a-40df-a168-eab5cd6512d0"}
-).json()["output"]
-
-print(r)
