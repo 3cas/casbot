6d77731f31e9edb0157c5eb576efd84bb4c47c4e
CAS <78571139+CAS-14@users.noreply.github.com>
2022-04-04 11:55:26 -0400

    Create r9k.py

---
+++ cb_ext/r9k.py
@@ -0,0 +1,37 @@
+from nextcord import *
+from nextcord.ext import commands
+
+import cb_ext.util as u
+
+class REAL9000(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(guild_ids=u.mains)  # Making the command and limiting the guilds
+    async def r9k(self, interaction: Interaction):
+        await interaction.response.send_message("Tested")
+
+    @r9k.subcommand(description="Test subcommand")  # Identifying The Sub-Command
+    async def subcommand_one(self, interaction: Interaction):  # Making The Sub Command Name And Passing Through Interaction
+        await interaction.response.send_message("This is subcommand 1!")  # Sending A Response
+
+    # Identifying The Sub-Command And Adding A Descripton
+    @r9k.subcommand()
+    async def subcommand_two(self, interaction: Interaction):  # Passing in interaction
+        await interaction.response.send_message("This is subcommand 2!")  # Responding with a message
+
+    @r9k.subcommand(description="Test menu")
+    async def choose_a_number(
+        self,
+        interaction: Interaction,
+        number: str = SlashOption(name="settings", description="Configure Your Settings", choices={"1": "one", "2": "two","3": "three"})):
+            await interaction.response.send_message(f"You chose {number}")
+
+    """
+    @user_command()
+    async def hello(self, interaction: Interaction, member: Member):
+        await interaction.response.send_message(f"Hello! {member}")
+    @message_command()
+    async def say(self, interaction: Interaction, message: Message):
+        await interaction.response.send_message(message.content, ephemeral=True)
+    """
