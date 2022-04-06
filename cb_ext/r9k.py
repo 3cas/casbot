8326d0b97963232fdc647f191e29dba8fc35f59c
CAS <78571139+cas-14@users.noreply.github.com>
2022-04-05 22:08:15 -0400

    fix

--- cb_ext/r9k.py
+++ cb_ext/r9k.py
@@ -4,33 +4,32 @@ from nextcord.ext import commands
 import cb_ext.util as u
 from cb_ext.util import db
 
+async def check(message, edited=False):
+    if message.channel.id == 960637529365831700:
+        ref = db.reference("/casbot/r9k/data")
+        content = message.content.lower()
+
+        if ";;;" in content:
+            await message.delete()
+    
+        else:
+            filtered = ''.join(filter(set('abcdefghijklmnopqrstuvwxyz').__contains__, message.content.lower()))
+            if len(filtered) == 0 or filtered in ref.get():
+                await message.delete()
+                # possibly add message or punishment here
+            else:
+                new_data = ref.get() + filtered + ";;;"
+                print(new_data)
+                ref.set(new_data)
+
 class REAL9000(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def check(message):
-        if message.channel.id == 960637529365831700:
-            ref = db.reference("/casbot/r9k/data")
-            content = message.content.lower()
-            print(ref.get())
-            print(content)
-
-            if ";;;" in content:
-                await message.delete()
-        
-            else:
-                filtered = ''.join(filter(set('abcdefghijklmnopqrstuvwxyz').__contains__, message.content.lower()))
-                if filtered.len() == 0 or filtered in ref.get():
-                    await message.delete()
-                    # possibly add message or punishment here
-                else:
-                    new_data = ref.get() + filtered + ";;;"
-                    ref.set(".", new_data)
-
     @commands.Cog.listener("on_message")
     async def check_sent(self, message):
-        await self.check(message)
+        await check(message)
 
     @commands.Cog.listener("on_message_edited")
     async def check_edited(self, message):
-        await self.check(message, True)
+        await check(message, True)
--- dbtest.py
+++
@@ -1,5 +0,0 @@
-from cb_ext.util import *
-
-ref = db.reference("/casbot/r9k/data")
-
-print(ref.get())
