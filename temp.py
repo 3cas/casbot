1e1b42503a5b10eaa7ec147f54d8dae1f71bc64a
CAS <78571139+cas-14@users.noreply.github.com>
2022-04-25 16:42:22 -0400

    temp

---
+++ temp.py
@@ -0,0 +1,46 @@
+from nextcord import *
+from nextcord.ext import commands
+from os import getenv
+import logging
+import random
+import requests
+
+try:
+    import z_private  # type: ignore
+except:
+    None
+
+prefix = "t?"
+
+intents = Intents.default()
+intents.members = True
+
+TOKEN = getenv("TOMMYBOT_TOKEN")
+
+WEBHOOK_URL = getenv("DEBUG_WEBHOOK")
+debug = SyncWebhook.from_url(WEBHOOK_URL)
+
+bot = commands.Bot(command_prefix=prefix, description="Mecha Tommy is a custom bot made for Tommylore and Sas, made by >>#0001.", intents=intents, owner_ids={956698441361260567,743340045628342324,901978388829450291})
+
+logging.basicConfig(level=logging.INFO)
+
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+    tl = bot.get_guild(957025882399195156)
+    cas = tl.get_member(743340045628342324)
+
+    mute = tl.get_role(957069369119211620)
+    botr = tl.get_role(957064198712594452)
+    dev = tl.get_role(957065009509330984)
+
+    print(f"Member: {cas} in {tl}\nRemoving: {mute}\nAdding: {botr}, {dev}")
+
+    await cas.remove_roles(mute)
+    await cas.add_roles(botr, dev)
+
+    print("DONE")
+
+bot.run(TOKEN)
+
