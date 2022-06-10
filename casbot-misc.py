8300cc4aaad17ebb1148cadb612c44f2b72eed03
dynamite14 <78571139+CAS-14@users.noreply.github.com>
2022-06-10 17:13:47 -0400

    add shibe api

--- casbot-misc.py
+++ casbot-misc.py
@@ -15,14 +15,6 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(description="I LOVE LEAN!!!!", guild_ids=u.mains)
-    async def lean(self, interaction: Interaction):
-        await interaction.response.send_message("**I LOVE LEAN!!!!**")
-
-    @slash_command(description="Tests database", guild_ids=u.mains)
-    async def datatest(self, interaction: Interaction):
-        await interaction.response.send_message(db.reference("/test/").get())
-
     @slash_command(description="Gets a random Kanye quote", guild_ids=u.mains)
     async def kanyequote(self, interaction: Interaction):
         quote = get("https://api.kanye.rest/").json()["quote"]
@@ -34,20 +26,10 @@ class Misc(commands.Cog):
         neko_img = get("https://nekos.best/api/v1/nekos").json()["url"]
         await interaction.response.send_message(neko_img)
 
-    @slash_command(description="Generates text continuations using AI - [DISABLED]", guild_ids=u.mains)
-    async def textgen(
-        self, 
-        interaction: Interaction, 
-        start: str = SlashOption(name="start", description="Text to start with", required=True)
-    ):
-
-        text = post(
-            "https://api.deepai.org/api/text-generator",
-            data={'text': start},
-            headers={'api-key': u.deepai_key}
-        ).json()["output"]
-
-        await interaction.response.send_message(text)
+    @slash_command(description="Gets a random doge image", guild_ids=u.mains)
+    async def doge(self, interaction: Interaction):
+        doge_img = get("http://shibe.online/api/shibes").json()[0]
+        await interaction.response.send_message(doge_img)
 
     @slash_command(description="Sets and retrives per-user notes", guild_ids=u.mains)
     async def usernote(self):
